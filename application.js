1e2173021eeb1a54661a450f93c1e79029e5cc9f
John <johnmarksjr@gmail.com>
2014-01-24 16:30:00 -0700

    Implemented adding shares to portfolio
    Can add shares of a stock
    Can delete a stock in the portfolio

--- application.js
+++ application.js
@@ -54,8 +54,46 @@ MarkitOnDemand.QuoteService.prototype.requestJsonpQuote = function(fCallback) {
  * Define the Markit On Demand chart service
  */
 
+var Utils = {}
+Utils.isNumeric = function(n) {
+	  return !isNaN(parseFloat(n)) && isFinite(n);
+}
 
 $(document).ready(function() {
+
+	function updateTotalValue() {
+		var total = parseFloat(0.0), stockValue = 0.0;
+		$('#stockList .stockValue').each(function() {
+			if ($(this).parent().find('.stockName').text() != "") {
+				var fieldVal = $(this).text();
+				if (Utils.isNumeric(fieldVal)) {
+					total = parseFloat(total) + parseFloat(fieldVal);
+				}
+			}
+		});
+		$('#stockList .stockValue.dummy').text(total.toFixed(2));
+	}
+
+	function updateStockValue(event) {
+		try {
+			var numShares = Number($(this).val()),
+	            price = 0,
+				value = 0,
+				list = '';
+
+			if (!$.isNumeric(numShares) || numShares.length == 0 || numShares <= 0)
+				throw new Error("Invalid number entered in # shares field");
+
+			list = $(this).parent();
+			price = $(list).find('.stockPrice').text();
+			value = (price * numShares).toFixed(2);
+			$(list).find('.stockValue').text(value);
+			updateTotalValue();
+			return false;
+		} catch(e) {
+			console.log(e.name +': '+ e.message);
+		}
+	}
 
 	function addSymbol(symbol, quote) {
 		var symbol = quote.Symbol;
@@ -63,17 +101,24 @@ $(document).ready(function() {
 		var change = quote.Change.toFixed(2);
 		var changePcnt = quote.ChangePercent.toFixed(2);
 
+		// Create the stock item
 		var itemHtml = "<li class='stockItem'>";
+		itemHtml += "<img src='trash.ico' class='show'>";
 		itemHtml += "<h2 class='stockName'>" + symbol + "</h2>";
 		itemHtml += "<h2 class='stockPrice anum'>" + price + "</h2>";
 		if (change >= 0)
 			itemHtml += "<h2 class='stockChange anum gain'>+" + change + "</h2>";
 		else
 			itemHtml += "<h2 class='stockChange anum loss'>" + change + "</h2>";
-		itemHtml += "<input type='text' class='numShares anum' name='numShares' placeholder='# shares'>";
+		itemHtml += "<input type='text' class='numShares anum' name='numShares' placeholder='# shares' autocomplete='off'>";
 		itemHtml += "<h2 class='stockValue anum'></h2>";
 		itemHtml += "</li>"
-		$(itemHtml).appendTo('#stockList');
+		var item = $(itemHtml).appendTo('#stockList');
+
+		// Add appropriate event handlers to the new list elements
+		//var stockName = $(item).find('.stockName').text();
+		var numShares = $(item).find('input');
+		$(numShares).change(updateStockValue);
 	}
 
 	function getQuote(symbol) {
@@ -82,10 +127,10 @@ $(document).ready(function() {
 				if (!jsonResult || jsonResult.Message) {
 					throw new Error(jsonResult.Message);
 				}
-				console.log(jsonResult);
+				//console.log(jsonResult);
 				addSymbol(symbol, jsonResult);
 			} catch(e) {
-				console.log(e.name +': '+ e.message);
+				//console.log(e.name +': '+ e.message);
 			}
 		});
 	}
@@ -111,14 +156,13 @@ $(document).ready(function() {
 
 	function updatePortfolio() {
 		try {
-			var symbol = $('#symbolLookup').val();
+			var symbol = $('#symbolLookup').val().toUpperCase();
 
 			if (!symbol.length)
 				throw {name:"Note", message:"Please enter a valid stock symbol"}; 
 
-			//var allNameElements = $('#stockList li h2.stockName').text();
 			$('#stockList li h2.stockName').each(function(index) {
-				var name = $(this).text();
+				var name = $(this).text().toUpperCase();
 				if (name == symbol)
 					throw {name:"Note", message:"Can't add symbol: the symbol is already in the portfolio"};
 			});
@@ -126,22 +170,28 @@ $(document).ready(function() {
 			// Use AJAX to get a clean stock quote
 			getQuote(symbol);
 		} catch(e) {
-			console.log(e);
+			//console.log(e.name +': '+ e.message);
 		}
 	}
 
-	/*$('form').submit(function() {
-		showQuote()
-	});*/
-
 	$('#addSymbol').click(function(event) {
 		updatePortfolio();
-		event.preventDefault();
 		$('#symbolLookup').val('');
+		event.preventDefault();
+		//event.stopPropagation();
 	});
 
 	$('#stockList').on('click', 'h2.stockName', function() {
 		selectStockItem($(this).parent(), $(this).text());
+	});
+
+	$('#stockList').on('click', 'img.show', function() {
+		var stock = $(this).parent();
+		var ans = confirm("Are you sure you want to remove this stock from the portfolio?");
+		if (ans==true) {
+			$(stock).remove();
+			updateTotalValue();
+		}
 	});
 
 	// jQuery UI code for tooltips
